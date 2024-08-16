4ea68f351961181fe0a1c2c4c6a435a15b95f6be
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2024-08-16 11:18:47 +0530

    add masala-ui workspace

---
+++ masala-ui/src/Errors.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Diagnostic } from "langium-ast-helper";
+
+interface DiagnosticProps {
+  diagnostics: Diagnostic[]
+}
+
+export default function Errors(props: DiagnosticProps) {
+  const { diagnostics } = props
+  return (
+    <div className="flex flex-col h-full w-full p-4 justify-start items-center my-10">
+      <div className="text-white border-2 border-solid border-accentRed rounded-md p-4 text-left text-sm cursor-default">
+        {diagnostics
+          .filter((i) => i.severity === 1)
+          .map((diagnostic, index) => (
+            <details key={index}>
+              <summary>{`Line ${diagnostic.range.start.line + 1}-${
+                diagnostic.range.end.line + 1
+              }: ${diagnostic.message}`}</summary>
+              <p>
+                Source: {diagnostic.source} | Code: {diagnostic.code}
+              </p>
+            </details>
+          ))}
+      </div>
+    </div>
+  );
+}
